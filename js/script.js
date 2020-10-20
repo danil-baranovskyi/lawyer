adbb362ef7a9b077542cc2d7c7201bcb01d9215d
danil-baranovskyi <63189564+danil-baranovskyi@users.noreply.github.com>
2020-10-20 15:25:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,51 @@
-'use srtict'
 const winWidth = window.innerWidth;
 const advItems = document.querySelectorAll('.advantages-item');
 const servicesItem = document.querySelectorAll('.services-variaty-item');
 const mobileMenu = document.querySelector('.mobile-menu');
 const overlay = document.querySelector('.overlay')
-let menuBtn = document.getElementById('menuBtn');
-let menuContainer = document.getElementById('menuContainer');
- 
- 
+const menuBtn = document.getElementById('menuBtn');
+const menuContainer = document.getElementById('menuContainer');
+const mainNavList = document.querySelectorAll('.scroll-item');
+const mobileNavList = document.querySelectorAll('.scroll-item-mob');
+
+const aboutMe = document.querySelector('.about-me');
+const services = document.querySelector('.services');
+const news = document.querySelector('.news');
+const contacts = document.querySelector('.form');
+const main = document.querySelector('.more');
+
+const scrollToItems =[
+    main,
+    aboutMe,
+    services,
+    news,
+    contacts,
+]
+
+const scrollTo = (el) =>{
+    el.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+    })
+}
+
 const burgerOpen = function() {
     menuBtn.classList.toggle('opened');
     mobileMenu.classList.toggle('mobile-menu-active');
     overlay.classList.toggle('overlay-active');
 }
+for (let i = 0; i < scrollToItems.length; i++){
+    mainNavList[i].addEventListener('click', () => {
+        scrollTo(scrollToItems[i])
+    })
+}
+for (let i = 0; i < scrollToItems.length; i++){
+    mobileNavList[i].addEventListener('click', () => {
+        scrollTo(scrollToItems[i]);
+    });
+}
+menuBtn.addEventListener('click', burgerOpen);
+overlay.addEventListener('click', burgerOpen);
 
 if(winWidth < 890){
     advItems[1].dataset.aos = "fade-left";
@@ -22,10 +55,6 @@ if (winWidth <= 1140){
     servicesItem[1].dataset.aos = "fade-down-left";
     servicesItem[2].dataset.aos = "zoom-in";
 }
-
-menuBtn.addEventListener('click', burgerOpen);
-overlay.addEventListener('click', burgerOpen);
-
 AOS.init({
     disable: function () {
         let maxWidth = 730;
